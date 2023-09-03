24dbd969581a9068978833c410a1d73f5e9b56df
Brian Nezhad <briannezhad@gmail.com>
2023-09-03 10:32:31 -0400

    Rename Boilerplate to Telow Client

--- telow-client/telow-client.php
+++ telow-client/telow-client.php
@@ -9,19 +9,19 @@
  * that starts the plugin.
  *
  * @link              https://telow.com
- * @since             1.0.0
+ * @since             0.0.1
  * @package           Telow Client
  *
  * @wordpress-plugin
- * Plugin Name:       WordPress Plugin Boilerplate
- * Plugin URI:        http://example.com/plugin-name-uri/
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.0
- * Author:            Your Name or Your Company
- * Author URI:        http://example.com/
+ * Plugin Name:       Telow Client
+ * Plugin URI:        http://telow.com/
+ * Description:       Telow Client is a WordPress plugin for tracking all your WP bugs and reporting to the Telow account.
+ * Version:           0.0.1
+ * Author:            Telow
+ * Author URI:        https://telow.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       plugin-name
+ * Text Domain:       telow-client
  * Domain Path:       /languages
  */
 
@@ -35,23 +35,23 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'PLUGIN_NAME_VERSION', '1.0.0' );
+define( 'TELOW_CLIENT', '0.0.1' );
 
 /**
  * The code that runs during plugin activation.
- * This action is documented in includes/class-plugin-name-activator.php
+ * This action is documented in includes/class-telow-client-activator.php
  */
 function activate_plugin_name() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-activator.php';
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-telow-client-activator.php';
 	Plugin_Name_Activator::activate();
 }
 
 /**
  * The code that runs during plugin deactivation.
- * This action is documented in includes/class-plugin-name-deactivator.php
+ * This action is documented in includes/class-telow-client-deactivator.php
  */
 function deactivate_plugin_name() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-deactivator.php';
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-telow-client-deactivator.php';
 	Plugin_Name_Deactivator::deactivate();
 }
 
@@ -62,7 +62,7 @@ register_deactivation_hook( __FILE__, 'deactivate_plugin_name' );
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
-require plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name.php';
+require plugin_dir_path( __FILE__ ) . 'includes/class-telow-client.php';
 
 /**
  * Begins execution of the plugin.
